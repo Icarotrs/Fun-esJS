4aaf7633600f5cb7c9e35cc5e97f32f8e36ccea0
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-01-16 19:38:11 -0300

    Update and rename Treinamento-funções.js to Treinamento1.js

--- Treinamento1.js
+++ Treinamento1.js
@@ -17,4 +17,13 @@ function parimpar(){
         alert("O numero é impar");
     }
 }
-parimpar();
+//parimpar();
+
+//Função que lê o salário de um funcionário e informa o valor com um aumento de 15%
+function salario(){
+    let salario = Number(prompt('Digite o valor do seu salário: '));
+    let novosalario = salario * 1.15;
+    alert(`Seu novo salário é de: ${novosalario}`);
+}
+//salario();
+
