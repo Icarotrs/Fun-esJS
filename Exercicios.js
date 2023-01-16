9f241c0ba99aba6254e494c390aece4dda1b51bc
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-01-16 20:25:33 -0300

    Create Exercicios.js

---
+++ Exercicios.js
@@ -0,0 +1,2 @@
+//Função que le uma mensagem Ola + nome
+// Existem 2 possibilidades: Deixar que o usuario defina o nome como em: prompt('Qual seu nome?'); ou definir o nome utilizando um parâmetro:
