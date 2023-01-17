f0e8def49d6020cadde59919c9da1c58c87ec454
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-01-16 21:13:37 -0300

    Update and rename Exercicios.js to Exercicio5.js

--- Exercicio5.js
+++ Exercicio5.js
@@ -1,2 +1,8 @@
 //Função que le uma mensagem Ola + nome
 // Existem 2 possibilidades: Deixar que o usuario defina o nome como em: prompt('Qual seu nome?'); ou definir o nome utilizando um parâmetro:
+
+//Exemplo utilizando somente o console para exibição:
+function ola(nome){
+    console.log('Ola' + nome);
+}
+ola(" Icaro")
