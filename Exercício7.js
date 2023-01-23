920f07139a06a65acfcd3ea94f5e45267b552171
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-01-23 20:31:01 -0300

    Update Exercício7.js

--- Exercício7.js
+++ Exercício7.js
@@ -1,14 +1,15 @@
 //função que aceita somente valores se B for maior que c e d e se a soma de c e d for maior que a e b.
-function senha(valores){
+function testarValores() {
     a = prompt('Digite o valor de A');
     b = prompt('Digite o Valor de B');
     c = prompt('Digite o valor de C');
     d = prompt('Digite o valor de D');
-    valores = [a,b,c,d]
-    if (b > c, b > d, c + d > a + b) {
-        alert (`valores aceitos`)
-    } else{
-        alert(`Valores negados`)
+    if ((b > c) && (d > a) && (c + d > a + b) && (c > 0) && (d > 0) && (a % 2 == 0)) {
+        alert('Valores aceitos!');
+    }
+    else {
+        alert('Valores não aceitos');
     }
 }
-senha();
+
+testarValores();
