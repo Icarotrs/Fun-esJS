d4a19f50b2be4605db0b413118a13a01edafddc1
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-01-23 20:19:48 -0300

    Update Exercicio7.js

--- Exercicio7.js
+++ Exercicio7.js
@@ -1,4 +1,4 @@
-função que aceita somente valores se B for maior que c e d e se a soma de c e d for maior que a e b.
+//função que aceita somente valores se B for maior que c e d e se a soma de c e d for maior que a e b.
 function senha(valores){
     a = prompt('Digite o valor de A');
     b = prompt('Digite o Valor de B');
