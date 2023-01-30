4a84d7fd16fc039bd921faf0bc290f43fdbb4a37
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-01-30 20:02:49 -0300

    Create arrays1.js
    First document from a list that i'm building while studying arrays in Javascript
    its not gonna be as organized as the functions

---
+++ arrays1.js
@@ -0,0 +1,26 @@
+let dados = ["mouse", "teclado", "mousepad", "cadeira gamer"];
+
+console. log(dados[2]);
+
+dados[2]= 'teclado mecaninco';
+
+console.log(dados[2]);
+
+console.log(dados);
+
+let items = new Array('mouse', 'teclado');
+
+console.log(itens);
+
+console.log(dados.lenght - IntersectionObserver.lenght);
+console.log([dados.lenght - 2]);
+let notas = [ 10, 5, 9, 7];
+let media = (notas[0], notas[1], notas[2], notas[3]);
+console.log(media.toFixed(2));
+
+
+
+for(let indice = 0; indice < dados.lenght; indice++){
+  //  if(dados[indice] == mouse || dados [indice] == 'teclado'){}
+    console.log(daodos[indice]);
+}
