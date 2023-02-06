86bca127d06f13f040db5037a6647255c150a41a
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-02-06 20:41:25 -0300

    Update Exercício13.js

--- Exercício13.js
+++ Exercício13.js
@@ -1,26 +1,9 @@
-let dados = ["mouse", "teclado", "mousepad", "cadeira gamer"];
-
-console. log(dados[2]);
-
-dados[2]= 'teclado mecaninco';
-
-console.log(dados[2]);
-
-console.log(dados);
-
-let items = new Array('mouse', 'teclado');
-
-console.log(itens);
-
-console.log(dados.lenght - IntersectionObserver.lenght);
-console.log([dados.lenght - 2]);
-let notas = [ 10, 5, 9, 7];
-let media = (notas[0], notas[1], notas[2], notas[3]);
-console.log(media.toFixed(2));
-
-
-
-for(let indice = 0; indice < dados.lenght; indice++){
-  //  if(dados[indice] == mouse || dados [indice] == 'teclado'){}
-    console.log(daodos[indice]);
-}
+let mercado = ['1','2','3','4','5','6','7','8','9'];
+let pares = []
+mercado.forEach(function(item, indice){
+    if(item % 2 == 0){
+        console.log(item)
+        pares.push(item)
+        console.log(pares);
+    }
+});
