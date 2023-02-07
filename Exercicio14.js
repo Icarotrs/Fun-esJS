4f7917ed9fa81032319055a0c2dd8bbdbd15eff4
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-02-06 21:58:02 -0300

    Create Exercicio14.js

---
+++ Exercicio14.js
@@ -0,0 +1,10 @@
+console.log(document.getElementsByTagName("div"));
+
+const cabecalho = document.getElementsByClassName('heading');
+console.log(cabecalho);
+
+const h1 = document.getElementById('cabecalho');
+console.log(h1);
+
+const div = document.querySelectorAll('li');
+console.log(div);
