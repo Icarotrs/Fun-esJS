692fa9e02a397880c4eae8c0453d9762ddcf68aa
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-02-13 20:21:03 -0300

    Update Exercício16.js

--- Exercício16.js
+++ Exercício16.js
@@ -1,12 +1,45 @@
-//console.log(document.body);
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[3]);
+console.log(document.body.childNodes[3].childNodes);
+console.log(document.body.childNodes[3].childNodes[1]);
+console.log(document.body.childNodes[3].childNodes[1].childNodes[1]);
+//console.log(document.body.childNodes[3].childNodes[1].childNodes[1].textContent);
+
+// Como pegar elementos 
+/**
+ * Pela tag
+ * -> document.getElementsByTagName('nome-da-tag')
+ * Pela class
+ * -> document.getElementsByClassName('nome-da-class')
+ * Pelo id
+ * -> document.getElementById('nome-do-id')
+ * Pelo seletor CSS
+ * -> document.querySelector('seletor-css')
+ * pela tag => nome-da-tag
+ * pela class => .nome-da-class
+ * pelo id => #nome-do-id
+ * 
+ * -- Pega o primeiro elemento elemento que ele encontrar com o seletor
+ */
+
 let h1 = document.getElementsByTagName('h1');
-//console.log(h1);
+console.log(h1);
 
-let headr = document.getElementsByClassName('header');
-//console.log('header');
+let header = document.getElementsByClassName('header');
+console.log(header);
 
-let titulo = document.getElementById('título');
-//console.log(titulo);
-//console.log('texto do <p> ' + titulo.textContent);
+let titulo = document.getElementById('titulo');
+console.log(titulo);
+console.log("Texto do <p> " + titulo.textContent);
+
+
+// Pegando os elementos com querySelector
 let h1q = document.querySelector('h1');
-//console.log(h1q);
+console.log(h1q);
+
+let headerQ = document.querySelector('.header');
+console.log(headerQ);
+
+let tituloQ = document.querySelector('#titulo');
+console.log(tituloQ);
+
