001bd5e0dd6b13f22680fd0ba001f398d7c5bd41
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-02-13 20:12:14 -0300

    Create Exercício16.js
    encontrando elementos e exibindo no console

---
+++ Exercício16.js
@@ -0,0 +1,12 @@
+//console.log(document.body);
+let h1 = document.getElementsByTagName('h1');
+//console.log(h1);
+
+let headr = document.getElementsByClassName('header');
+//console.log('header');
+
+let titulo = document.getElementById('título');
+//console.log(titulo);
+//console.log('texto do <p> ' + titulo.textContent);
+let h1q = document.querySelector('h1');
+//console.log(h1q);
