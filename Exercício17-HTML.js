900a1e9e084d2b0b9151a85d41e38ec98966d0a4
Pedro Ícaro <119450479+Icarotrs@users.noreply.github.com>
2023-02-13 22:13:13 -0300

    Create Exercício17-HTML.js

---
+++ Exercício17-HTML.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <label>
+        produto: <input type="text" id="produto">
+    </label>
+
+    <button onclick="formatar()">
+        Inserir
+    </button>
+    <h1>Carrinho</h1>
+    <ul>
+
+    </ul>
+    <script src="scripts.js"></script>
+</body>
+</html>
